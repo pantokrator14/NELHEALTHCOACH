b79e7e3503739373d1d33afe0ab8f6d91c4a66fd
= <=>
2025-07-20 17:21:30 -0400

    creating monorepo

---
+++ apps/api/middleware/auth.ts
@@ -0,0 +1,16 @@
+// apps/api/middleware/auth.ts
+import jwt from 'jsonwebtoken';
+
+export const authenticate = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  
+  if (!token) return res.status(401).json({ error: 'Acceso no autorizado' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next(); // Pasar al siguiente middleware
+  } catch (error) {
+    res.status(401).json({ error: 'Token inválido' });
+  }
+};
