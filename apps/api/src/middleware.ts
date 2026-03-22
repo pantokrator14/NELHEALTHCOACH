d012d1661c02c7300f181d0587e5ce144f8d6865
= <=>
2026-03-21 22:47:32 -0400

    adding landing to cors

--- apps/api/src/middleware.ts
+++ apps/api/src/middleware.ts
@@ -8,6 +8,7 @@ export function middleware(request: NextRequest) {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:3002',
+    'https://www.nelhealthcoach.com',
     'https://app.nelhealthcoach.com',
     'https://form.nelhealthcoach.com',
   ];
